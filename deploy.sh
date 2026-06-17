518d95a99a41e93d8dc7778905e8b9fe48448ab1
alex <uzuzalex@gmail.com>
2026-06-17 16:07:13 +0300

    fix: kill orphan next-server process on port 3000 before pm2 restart

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ rsync -avz --delete \
   "$LOCAL_PATH" "$SERVER:$REMOTE_PATH"
 
 echo "Сборка и перезапуск..."
-ssh "$SERVER" "cd $REMOTE_PATH && rm -rf .next && npm run build && (pm2 restart demo-behavora || pm2 start npm --name demo-behavora -- start) && pm2 save"
+ssh "$SERVER" "cd $REMOTE_PATH && rm -rf .next && npm run build && sudo ss -tlnp | grep ':3000' | grep -oP 'pid=\K[0-9]+' | xargs -r sudo kill && (pm2 restart demo-behavora || pm2 start npm --name demo-behavora -- start) && pm2 save"
 
 echo "Деплой завершён!"
