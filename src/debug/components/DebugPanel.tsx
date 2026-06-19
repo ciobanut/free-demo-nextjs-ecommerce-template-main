1e0b34bad4e6fc8131e8ba140f4f41f1ddf5dcd2
alex <uzuzalex@gmail.com>
2026-06-19 15:14:04 +0300

    fix: call usePageMetrics in DebugPanel so Overview tab populates

--- src/debug/components/DebugPanel.tsx
+++ src/debug/components/DebugPanel.tsx
@@ -4,10 +4,12 @@ import React, { useState } from 'react';
 import CompactView from './CompactView';
 import ExpandedView from './ExpandedView';
 import { useNetworkLogger } from '../hooks/useNetworkLogger';
+import { usePageMetrics } from '../hooks/usePageMetrics';
 
 const DebugPanel: React.FC = () => {
     const [isExpanded, setIsExpanded] = useState(false);
     useNetworkLogger();
+    usePageMetrics();
 
     return isExpanded
         ? <ExpandedView onCollapse={() => setIsExpanded(false)} />
